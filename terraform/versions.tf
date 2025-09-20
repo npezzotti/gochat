39b4b6f046d62c687bde2953cb658de05ef29e5b
npezzotti <nathan.pezzotti@hashicorp.com>
2025-09-19 21:23:31 -0400

    renames state bucket

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "gochat-terraform-state"
+    bucket       = "gochat-state"
     key          = "global/s3/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
